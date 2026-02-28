9bd9fea774f3a40d67befe45e534dac258fb8202
mauricioraupp <mauricio.p.raupp@gmail.com>
2026-02-28 13:49:42 -0300

    google auth and middleware

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+// import { NextResponse } from "next/server"
+// import type { NextRequest } from "next/server"
+// import { getToken } from "next-auth/jwt"
+
+// export async function middleware(req: NextRequest) {
+//   const token = await getToken({
+//     req,
+//     secret: process.env.NEXTAUTH_SECRET,
+//   })
+
+//   const { pathname } = req.nextUrl
+
+//   if (token && pathname === "/login" || token && pathname === "/register") {
+//     return NextResponse.redirect(new URL("/profile", req.url))
+//   }
+
+//   if (!token && pathname === "/profile" || !token && pathname === "/") {
+//     return NextResponse.redirect(new URL("/login", req.url))
+//   }
+
+//   return NextResponse.next()
+// }
+
+// export const config = {
+//   matcher: ["/login", "/register", "/profile/:path*"],
+// }
