4bff97868aabd7d089ec7f829d913fca95d67a80
mauricioraupp <mauricio.p.raupp@gmail.com>
2026-03-04 12:17:58 -0300

    schema updated

--- src/app/(championship)/my-championships/page.tsx
+++ src/app/(championship)/my-championships/page.tsx
@@ -1,11 +1,11 @@
-import ChampionshipCreated from "@/components/features/championships/ChampionshipCreated";
+import ChampionshipsCreated from "@/components/features/championships/ChampionshipCreated";
 
-export default function Home() {
+export default function MyChampionships() {
   return (
     <main className="min-h-screen ml-0 sm:ml-25 lg:ml-68 bg-neutral-150">
       <section className="flex flex-col gap-8 w-fit mx-auto sm:py-16 py-24">
         <h1 className="font-semibold text-xl">Torneios Criados</h1>
-        <ChampionshipCreated/>
+        <ChampionshipsCreated/>
         <button className="bg-zinc-950 text-white h-32 w-full rounded-md cursor-pointer">+ Criar Torneio</button>
       </section>
     </main>
